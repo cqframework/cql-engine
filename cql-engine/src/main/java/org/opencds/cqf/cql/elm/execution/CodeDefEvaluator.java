54a2e4fd6b133c242a725105d634c4969061f816
Chris Schuler <chrisshculer@databaseconsultinggroup.com>
2017-04-25 16:06:51 -0600

    Removed term svc requirement from CodeRef

--- cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CodeDefEvaluator.java
+++ cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CodeDefEvaluator.java
@@ -12,6 +12,6 @@ public class CodeDefEvaluator extends org.cqframework.cql.elm.execution.CodeDef 
     @Override
     public Object evaluate(Context context) {
         CodeSystemInfo info = (CodeSystemInfo) getCodeSystem().evaluate(context);
-        return context.resolveTerminologyProvider().lookup(new Code().withCode(this.getId()), info);
+        return new Code().withCode(this.getId()).withSystem(info.getId());
     }
 }
