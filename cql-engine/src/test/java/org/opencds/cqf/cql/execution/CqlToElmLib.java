37e76558872c9427c47bc9e59cc0a511f61f947b
Darren Duncan <darren@DarrenDuncan.net>
2017-11-21 20:21:02 -0800

    split CqlToElmLib.java off from CqlRunnerLib.java

---
+++ cql-engine/src/test/java/org/opencds/cqf/cql/execution/CqlToElmLib.java
@@ -0,0 +1,67 @@
+package org.opencds.cqf.cql.execution;
+
+import java.util.ArrayList;
+
+import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.CqlTranslatorException;
+import org.cqframework.cql.cql2elm.LibraryManager;
+import org.cqframework.cql.cql2elm.ModelManager;
+import org.cqframework.cql.elm.tracking.TrackBack;
+
+/**
+ * Created by Darren on 2017-11-15.
+ */
+public class CqlToElmLib
+{
+    public static String maybe_cql_to_elm_xml(ArrayList<String> errors,
+        String maybe_cql_source_code)
+    {
+        ModelManager modelManager = new ModelManager();
+        LibraryManager libraryManager = new LibraryManager(modelManager);
+
+        ArrayList<CqlTranslator.Options> options = new ArrayList<>();
+        options.add(CqlTranslator.Options.EnableDateRangeOptimization);
+
+        CqlTranslator translator = CqlTranslator.fromText(
+            maybe_cql_source_code, modelManager, libraryManager,
+            options.toArray(new CqlTranslator.Options[options.size()]));
+
+        if (translator.getErrors().size() > 0)
+        {
+            collect_errors(errors, translator.getErrors());
+            return null;
+        }
+
+        String elm_xml = translator.toXml();
+
+        if (translator.getErrors().size() > 0)
+        {
+            collect_errors(errors, translator.getErrors());
+            return null;
+        }
+
+        return elm_xml;
+    }
+
+    public static void collect_errors(ArrayList<String> errors,
+        Iterable<CqlTranslatorException> exceptions)
+    {
+        errors.add("Translation of CQL to ELM failed due to errors:");
+        for (CqlTranslatorException error : exceptions)
+        {
+            TrackBack tb = error.getLocator();
+            String lines = tb == null ? "[n/a]" : String.format(
+                "%s[%d:%d, %d:%d]",
+                (tb.getLibrary() == null ? ""
+                    : tb.getLibrary().getId()
+                        + (tb.getLibrary().getVersion() == null ? ""
+                            : "-" + tb.getLibrary().getVersion()
+                        )
+                ),
+                tb.getStartLine(), tb.getStartChar(),
+                tb.getEndLine(), tb.getEndChar()
+            );
+            errors.add(lines + error.getMessage());
+        }
+    }
+}
