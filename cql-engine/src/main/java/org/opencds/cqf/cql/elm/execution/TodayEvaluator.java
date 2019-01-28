3bcfce576a324d1dfb43de86738924366c6c7d5f
Chris Schuler <cschuler@myharmoniq.com>
2019-01-28 15:23:49 -0700

    #161: Fixed datetime operator tests ... Updated Today and DateFrom operators to return a Date

--- cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/TodayEvaluator.java
+++ cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/TodayEvaluator.java
@@ -3,19 +3,16 @@ package org.opencds.cqf.cql.elm.execution;
 import org.opencds.cqf.cql.execution.Context;
 
 /*
-Today() DateTime
+Today() Date
 
 The Today operator returns the date (with no time component) of the start timestamp associated with the evaluation request.
 See the Now operator for more information on the rationale for defining the Today operator in this way.
 */
 
-/**
-* Created by Chris Schuler on 6/21/2016
-*/
 public class TodayEvaluator extends org.cqframework.cql.elm.execution.Today {
 
   @Override
   public Object evaluate(Context context) {
-    return context.logTrace(this.getClass(), DateFromEvaluator.dateFrom(context.getEvaluationDateTime()));
+    return DateFromEvaluator.dateFrom(context.getEvaluationDateTime());
   }
 }
