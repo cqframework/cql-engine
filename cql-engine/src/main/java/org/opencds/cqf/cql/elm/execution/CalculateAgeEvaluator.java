b132dd347b6e908d0d0251c658380921e556e385
Chris Schuler <cschuler@myharmoniq.com>
2019-06-03 09:10:25 -0600

    #174: Accounting for possible Date operand in the CalculateAgeEvaluator

--- cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CalculateAgeEvaluator.java
+++ cql-engine/src/main/java/org/opencds/cqf/cql/elm/execution/CalculateAgeEvaluator.java
@@ -1,32 +1,39 @@
 package org.opencds.cqf.cql.elm.execution;
 
 import org.opencds.cqf.cql.execution.Context;
+import org.opencds.cqf.cql.runtime.Date;
 import org.opencds.cqf.cql.runtime.DateTime;
 
 import java.lang.reflect.InvocationTargetException;
 
 // for Uncertainty
 /*
+CalculateAgeInYears(birthDate Date) Integer
 CalculateAgeInYears(birthDate DateTime) Integer
+CalculateAgeInMonths(birthDate Date) Integer
 CalculateAgeInMonths(birthDate DateTime) Integer
+CalculateAgeInWeeks(birthDate Date) Integer
+CalculateAgeInWeeks(birthDate DateTime) Integer
+CalculateAgeInDays(birthDate Date) Integer
 CalculateAgeInDays(birthDate DateTime) Integer
 CalculateAgeInHours(birthDate DateTime) Integer
 CalculateAgeInMinutes(birthDate DateTime) Integer
 CalculateAgeInSeconds(birthDate DateTime) Integer
 
-The CalculateAge operators calculate the age of a person born on the given birthdate as of now in the precision named in the operator.
+The CalculateAge operators calculate the age of a person born on the given birth date/time
+    as of today/now in the precision named in the operator.
+
 If the birthdate is null, the result is null.
-The CalculateAge operators are defined in terms of a DateTime duration calculation.
-  This means that if the given birthDate is not specified to the level of precision corresponding to the operator being invoked,
-    the result will be an uncertainty over the range of possible values, potentially causing some comparisons to return null.
+
+The CalculateAge operators are defined in terms of a date/time duration calculation.
+    This means that if the given birthDate is not specified to the level of precision corresponding
+        to the operator being invoked, the result will be an uncertainty over the range of possible values,
+        potentially causing some comparisons to return null.
 */
 
-/**
- * Created by Chris Schuler on 7/14/2016
- */
 public class CalculateAgeEvaluator extends org.cqframework.cql.elm.execution.CalculateAge {
 
-    public static Object calculateAge(Object operand, String precision, DateTime today) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public static Object calculateAge(Object operand, String precision, Object today) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
         if (operand == null) {
             return null;
         }
@@ -39,9 +46,17 @@ public class CalculateAgeEvaluator extends org.cqframework.cql.elm.execution.Cal
         Object operand = getOperand().evaluate(context);
         String precision = getPrecision().value();
 
-        try {
-            return context.logTrace(this.getClass(), calculateAge(operand, precision, context.getEvaluationDateTime()), operand);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+        Object today =
+                operand instanceof Date
+                        ? DateFromEvaluator.dateFrom(context.getEvaluationDateTime())
+                        : context.getEvaluationDateTime();
+
+        try
+        {
+            return calculateAge(operand, precision, today);
+        }
+        catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e)
+        {
             e.printStackTrace();
             throw new RuntimeException(e.getMessage());
         }
