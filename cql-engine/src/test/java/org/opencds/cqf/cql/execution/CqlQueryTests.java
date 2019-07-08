dcaf233e1f4bb68f3e3a50367befbc69f2b451d0
Chris Schuler <cschuler@myharmoniq.com>
2019-07-07 18:29:02 -0600

    204: clearing let cache for every query - wrote test to confirm example in issue is evaluating correctly

---
+++ cql-engine/src/test/java/org/opencds/cqf/cql/execution/CqlQueryTests.java
@@ -0,0 +1,22 @@
+package org.opencds.cqf.cql.execution;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class CqlQueryTests extends CqlExecutionTestBase
+{
+    @Test
+    public void TestLet()
+    {
+        Context context = new Context(library);
+
+        Object result = context.resolveExpressionRef("RightShift").getExpression().evaluate(context);
+        Assert.assertEquals(result, Arrays.asList(null, "A", "B", "C"));
+        result = context.resolveExpressionRef("LeftShift").getExpression().evaluate(context);
+        Assert.assertEquals(result, Arrays.asList("B", "C", "D", null));
+        result = context.resolveExpressionRef("LeftShift2").getExpression().evaluate(context);
+        Assert.assertEquals(result, Arrays.asList("B", "C", "D", null));
+    }
+}
