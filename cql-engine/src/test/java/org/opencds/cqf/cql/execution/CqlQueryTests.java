048884c4db1061c60b6de11841e7c69a643e1286
Chris Schuler <cschuler@myharmoniq.com>
2019-07-21 07:19:15 -0600

    #152: fixing bug in relationship resolution and adding tests for without relationship

--- cql-engine/src/test/java/org/opencds/cqf/cql/execution/CqlQueryTests.java
+++ cql-engine/src/test/java/org/opencds/cqf/cql/execution/CqlQueryTests.java
@@ -19,4 +19,13 @@ public class CqlQueryTests extends CqlExecutionTestBase
         result = context.resolveExpressionRef("LeftShift2").getExpression().evaluate(context);
         Assert.assertEquals(result, Arrays.asList("B", "C", "D", null));
     }
+
+    @Test
+    public void TestWithout()
+    {
+        Context context = new Context(library);
+
+        Object result = context.resolveExpressionRef("ClaimWithQualifiyingPOSWithoutEncounter").getExpression().evaluate(context);
+        Assert.assertTrue(result == null);
+    }
 }
