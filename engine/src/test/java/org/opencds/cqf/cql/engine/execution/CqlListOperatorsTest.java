d139b256880cd490719e41a5b25ce44de545eb5d
Jonathan Percival <jonathan.i.percival@gmail.com>
2020-03-24 19:47:24 -0600

    Fix failing tests

--- engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlListOperatorsTest.java
+++ engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlListOperatorsTest.java
@@ -84,9 +84,9 @@ public class CqlListOperatorsTest extends CqlExecutionTestBase {
 
         result = context.resolveExpressionRef("timeList").getExpression().evaluate(context);
         List<Time> arrList = new ArrayList<>();
-        arrList.add(new Time(offset, 15, 59, 59, 999));
-        arrList.add(new Time(offset, 15, 12, 59, 999));
-        arrList.add(new Time(offset, 15, 12, 13, 999));
+        arrList.add(new Time(15, 59, 59, 999));
+        arrList.add(new Time(15, 12, 59, 999));
+        arrList.add(new Time(15, 12, 13, 999));
         Assert.assertTrue(EquivalentEvaluator.equivalent(result, arrList));
     }
 
@@ -136,7 +136,7 @@ public class CqlListOperatorsTest extends CqlExecutionTestBase {
     /**
      * {@link org.opencds.cqf.cql.engine.elm.execution.DistinctEvaluator#evaluate(Context)}
      */
-    @Test    
+    @Test
     @SuppressWarnings("serial")
     public void testDistinct() throws JAXBException {
         Context context = new Context(library);
