8188fcd8436af3c67baae249f2fbfd4be3ca0842
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2020-04-18 12:36:30 -0600

    More general approach to type serialization with Jackson

--- engine/src/main/java/org/opencds/cqf/cql/engine/execution/JsonCqlLibraryReader.java
+++ engine/src/main/java/org/opencds/cqf/cql/engine/execution/JsonCqlLibraryReader.java
@@ -14,11 +14,11 @@ public class JsonCqlLibraryReader {
         ObjectMapper mapper = new ObjectMapper();
         JaxbAnnotationModule module = new JaxbAnnotationModule();
         mapper.registerModule(module);
-        SimpleModule simpleModule = new SimpleModule();
-        simpleModule.setMixInAnnotation(Expression.class, org.opencds.cqf.cql.elm.execution.ExpressionMixin.class);
-        simpleModule.setMixInAnnotation(TypeSpecifier.class, org.opencds.cqf.cql.elm.execution.TypeSpecifierMixin.class);
-        simpleModule.setMixInAnnotation(ExpressionDef.class, org.opencds.cqf.cql.elm.execution.ExpressionDefMixin.class);
-        mapper.registerModule(simpleModule);
+        //SimpleModule simpleModule = new SimpleModule();
+        //simpleModule.setMixInAnnotation(Expression.class, org.opencds.cqf.cql.elm.execution.ExpressionMixin.class);
+        //simpleModule.setMixInAnnotation(TypeSpecifier.class, org.opencds.cqf.cql.elm.execution.TypeSpecifierMixin.class);
+        //simpleModule.setMixInAnnotation(ExpressionDef.class, org.opencds.cqf.cql.elm.execution.ExpressionDefMixin.class);
+        //mapper.registerModule(simpleModule);
         Library result = mapper.readValue(reader, LibraryWrapper.class).getLibrary();
         return result;
     }
