bd8cc478428f6750821a6865c417f04c646a1684
Jonathan Percival <jonathan.i.percival@gmail.com>
2020-10-09 12:41:55 -0600

    Add namespace parsing to include def resolution

--- engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlEngineTests.java
+++ engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlEngineTests.java
@@ -5,11 +5,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,61 +14,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 
-import org.cqframework.cql.cql2elm.CqlTranslator;
 import org.cqframework.cql.cql2elm.CqlTranslatorException;
 import org.cqframework.cql.cql2elm.CqlTranslatorOptions;
 import org.cqframework.cql.cql2elm.LibraryManager;
-import org.cqframework.cql.cql2elm.ModelManager;
 import org.cqframework.cql.cql2elm.model.TranslatedLibrary;
 import org.cqframework.cql.elm.execution.Library;
-import org.hl7.elm.r1.ObjectFactory;
 import org.junit.Test;
 
-public class CqlEngineTests {
-
-    private LibraryManager toLibraryManager(Map<org.hl7.elm.r1.VersionedIdentifier, String> libraryText) throws IOException, JAXBException {
-        ModelManager modelManager = new ModelManager();
-        LibraryManager libraryManager = new LibraryManager(modelManager);
-        libraryManager.getLibrarySourceLoader().registerProvider(new InMemoryLibrarySourceProvider(libraryText));
-        return libraryManager;
-    }
-
-    private Library toLibrary(String text) throws IOException, JAXBException  {
-        ModelManager modelManager = new ModelManager();
-        LibraryManager libraryManager = new LibraryManager(modelManager);
-
-        return this.toLibrary(text, modelManager, libraryManager);
-    }
-
-    private Library toLibrary(String text, ModelManager modelManager, LibraryManager libraryManager) throws IOException, JAXBException {
-        CqlTranslator translator = CqlTranslator.fromText(text, modelManager, libraryManager);
-        return this.readXml(translator.toXml());
-    }
-
-    private Library readXml(String xml) throws IOException, JAXBException {
-        return CqlLibraryReader.read(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)));
-    }
-
-    private org.hl7.elm.r1.VersionedIdentifier toElmIdentifier(String name, String version) {
-        return new org.hl7.elm.r1.VersionedIdentifier().withId(name).withVersion(version);
-    }
-
-    public String convertToXml(org.hl7.elm.r1.Library library) throws JAXBException {
-        Marshaller marshaller = getJaxbContext().createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        StringWriter writer = new StringWriter();
-        marshaller.marshal(new ObjectFactory().createLibrary(library), writer);
-        return writer.getBuffer().toString();
-    }
-
-    public static JAXBContext getJaxbContext() throws JAXBException {
-        return JAXBContext.newInstance(org.hl7.elm.r1.Library.class, org.hl7.cql_annotations.r1.Annotation.class);
-    }
-
+public class CqlEngineTests extends TranslatingTestBase {
 
     
     @Test(expected = IllegalArgumentException.class)
