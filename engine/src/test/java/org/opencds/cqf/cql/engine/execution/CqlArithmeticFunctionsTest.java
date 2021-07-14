24f329681f80f45275429f8e22816874150b5160
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2021-07-14 10:48:01 -0600

    long Minimum evaluation in engine

--- engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlArithmeticFunctionsTest.java
+++ engine/src/test/java/org/opencds/cqf/cql/engine/execution/CqlArithmeticFunctionsTest.java
@@ -380,6 +380,9 @@ public class CqlArithmeticFunctionsTest extends CqlExecutionTestBase {
         Object result = context.resolveExpressionRef("IntegerMinValue").getExpression().evaluate(context);
         assertThat(result, is(Integer.MIN_VALUE));
 
+        result = context.resolveExpressionRef("LongMinValue").getExpression().evaluate(context);
+        assertThat(result, is(Long.MIN_VALUE));
+
         result = context.resolveExpressionRef("DecimalMinValue").getExpression().evaluate(context);
         Assert.assertTrue(((BigDecimal) result).compareTo(new BigDecimal("-9999999999999999999999999999.99999999")) == 0);
 
