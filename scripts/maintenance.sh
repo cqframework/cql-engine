2b0afe6e52d226142fd6a42b3ae19fa7329ed57f
Vitor Pamplona <vitor@vitorpamplona.com>
2021-12-07 09:52:30 -0500

    Reverting JAXB dependencies to 0.12.0 due to a required alignment with FHIR dependencies.

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -4,4 +4,4 @@
 set -euxo pipefail
 bash -n "$0"
 
-mvn versions:update-properties versions:use-releases versions:use-latest-releases -Dexcludes=org.jvnet.jaxb2_commons:jaxb2-basics:jar:0.12.0,org.eclipse.persistence:org.eclipse.persistence.moxy:jar:2.7.7,org.slf4j:\*
+mvn versions:update-properties versions:use-releases versions:use-latest-releases -Dexcludes=org.jvnet.jaxb2_commons:jaxb2-basics:jar:1.11.1,org.eclipse.persistence:org.eclipse.persistence.moxy:jar:2.7.7,org.slf4j:\*
