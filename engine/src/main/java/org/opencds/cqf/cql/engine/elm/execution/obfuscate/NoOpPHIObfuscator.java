f9843839b4070f376a074f728f3d8f1b0e981c2f
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-12-15 10:37:21 -0700

    Fix build, use spot bugs

--- engine/src/main/java/org/opencds/cqf/cql/engine/elm/execution/obfuscate/NoOpPHIObfuscator.java
+++ engine/src/main/java/org/opencds/cqf/cql/engine/elm/execution/obfuscate/NoOpPHIObfuscator.java
@@ -1,11 +1,11 @@
 package org.opencds.cqf.cql.engine.elm.execution.obfuscate;
 
-import javax.annotation.Nullable;
+
 
 public class NoOpPHIObfuscator implements PHIObfuscator {
 
     @Override
-    public String obfuscate(@Nullable Object source) {
+    public String obfuscate(Object source) {
         return String.valueOf(source);
     }
 
