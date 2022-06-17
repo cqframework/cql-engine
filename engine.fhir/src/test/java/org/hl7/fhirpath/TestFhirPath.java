9fd50fcf72f362331c1c00737eeac7bdade934f6
Bryn Rhodes <bryn@databaseconsultinggroup.com>
2022-06-17 14:22:49 -0600

    #578: Fixed now and today not resolving appropriately in FHIRPath expressions.

--- engine.fhir/src/test/java/org/hl7/fhirpath/TestFhirPath.java
+++ engine.fhir/src/test/java/org/hl7/fhirpath/TestFhirPath.java
@@ -483,15 +483,15 @@ public class TestFhirPath {
         int skipCounter = 0;
         int passCounter = 0;
         for (Group group : tests.getGroup()) {
-            if (group.getName() == null || !group.getName().equals("testNow")) {
-                continue;
-            }
+            //if (group.getName() == null || !group.getName().equals("testTimeOfDay")) {
+            //    continue;
+            //}
             System.out.println(String.format("Running test group %s...", group.getName()));
 
             for (org.hl7.fhirpath.tests.Test test : group.getTest()) {
-                if (test.getName() == null || !test.getName().equals("testNow1")) {
-                    continue;
-                }
+                //if (test.getName() == null || !test.getName().equals("testNow1")) {
+                //    continue;
+                //}
 
                 testCounter += 1;
                 try {
@@ -519,7 +519,7 @@ public class TestFhirPath {
 
     @Test
     public void testFhirPathR4() {
-        runTests("r4/tests-fhir-r4.xml", 721, 550, 18);
+        runTests("r4/tests-fhir-r4.xml", 724, 561, 18);
     }
 
     @Test
