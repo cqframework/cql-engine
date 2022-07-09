5de3b2ea97695aecd3e70b3e057f81a7f9cb28fd
Jonathan Percival <jonathan.i.percival@gmail.com>
2022-07-09 15:38:31 -0600

    Add additional context for terminology exceptions

---
+++ engine/src/main/java/org/opencds/cqf/cql/engine/exception/TerminologyProviderException.java
@@ -0,0 +1,40 @@
+package org.opencds.cqf.cql.engine.exception;
+
+import org.opencds.cqf.cql.engine.debug.SourceLocator;
+/**
+ * This class is meant to be thrown by implementations of the TerminologyProvider interface whenever they encounter an Exception
+ */
+public class TerminologyProviderException extends CqlException {
+
+    public TerminologyProviderException (String message)
+    {
+        super(message);
+    }
+    public TerminologyProviderException (String message, Throwable cause) {
+        super(message, cause);
+    }
+    public TerminologyProviderException (Throwable cause) {
+        super(cause);
+    }
+
+    public TerminologyProviderException (String message, SourceLocator sourceLocator) {
+        super(message, sourceLocator);
+    }
+
+    public TerminologyProviderException (String message, Throwable cause, SourceLocator sourceLocator) {
+        super(message, cause, sourceLocator);
+    }
+
+    public TerminologyProviderException (Throwable cause, SourceLocator sourceLocator) {
+        super(cause, sourceLocator);
+    }
+
+    public TerminologyProviderException (String message, SourceLocator sourceLocator, Severity severity) {
+        super(message, sourceLocator, severity);
+    }
+
+    public TerminologyProviderException (String message, Throwable cause, SourceLocator sourceLocator, Severity severity) {
+        super(message, cause, sourceLocator, severity);
+    }
+
+}
