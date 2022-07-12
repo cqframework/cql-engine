81ca983114d9f968c1ac317d012bebc062d0b2f0
Jreyno77 <jreysteven1@gmail.com>
2022-07-12 16:56:08 -0600

    QuantityType does not exist.  Quantity is a Type.

--- engine.fhir/src/main/java/org/opencds/cqf/cql/engine/fhir/converter/BaseFhirTypeConverter.java
+++ engine.fhir/src/main/java/org/opencds/cqf/cql/engine/fhir/converter/BaseFhirTypeConverter.java
@@ -198,7 +198,7 @@ abstract class BaseFhirTypeConverter implements FhirTypeConverter {
             case "DateTimeType": return toCqlDateTime((IPrimitiveType<java.util.Date>) value);
             case "TimeType": return toCqlTime((IPrimitiveType<String>) value);
             case "StringType": return toCqlString((IPrimitiveType<String>)value);
-            case "QuantityType": return toCqlQuantity((ICompositeType) value);
+            case "Quantity": return toCqlQuantity((ICompositeType) value);
             case "RatioType": return toCqlRatio((ICompositeType) value);
             case "AnyType": return toCqlAny((IBase)value);
             case "Coding": return toCqlCode((IBaseCoding) value);
