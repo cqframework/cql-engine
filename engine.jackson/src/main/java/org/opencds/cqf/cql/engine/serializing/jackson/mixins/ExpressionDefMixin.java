74cb3eb164dc8ee8af861ee41bc05b96478bace9
Vitor Pamplona <vitor@vitorpamplona.com>
2022-08-16 17:10:03 -0400

    No need for a class to define Mixins.

--- engine.jackson/src/main/java/org/opencds/cqf/cql/engine/serializing/jackson/mixins/ExpressionDefMixin.java
+++ engine.jackson/src/main/java/org/opencds/cqf/cql/engine/serializing/jackson/mixins/ExpressionDefMixin.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-import org.cqframework.cql.elm.execution.ExpressionDef;
 import org.opencds.cqf.cql.engine.elm.execution.ExpressionDefEvaluator;
 
 @JsonTypeInfo(use = Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type", defaultImpl = ExpressionDefEvaluator.class)
@@ -13,5 +12,5 @@ import org.opencds.cqf.cql.engine.elm.execution.ExpressionDefEvaluator;
     @Type(value = ExpressionDefEvaluator.class, name = "ExpressionDef")
     // @Type(value = FunctionDefEvaluator.class, name = "FunctionDef"),
 })
-public class ExpressionDefMixin extends ExpressionDef {
+public interface ExpressionDefMixin {
 }
