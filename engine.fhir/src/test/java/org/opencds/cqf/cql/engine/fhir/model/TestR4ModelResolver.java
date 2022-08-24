ac9e80c85f1ba03ee233f2d8f0d8c81948c8d1a3
Vitor Pamplona <vitor@vitorpamplona.com>
2022-08-24 12:57:22 -0400

    Activate service loaders (#603)
    * Activates service loaders.
    
    * Moves the engine to use Translator 2.1.0
    
    * Renaming VersionIdentifier to ModelIdentifier
    
    * Adding javax.json when using eclipse.moxy

--- engine.fhir/src/test/java/org/opencds/cqf/cql/engine/fhir/model/TestR4ModelResolver.java
+++ engine.fhir/src/test/java/org/opencds/cqf/cql/engine/fhir/model/TestR4ModelResolver.java
@@ -15,7 +15,7 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.FhirVersionEnum;
 import org.cqframework.cql.cql2elm.ModelManager;
 import org.cqframework.cql.cql2elm.model.Model;
-import org.hl7.elm.r1.VersionedIdentifier;
+import org.hl7.cql.model.ModelIdentifier;
 import org.hl7.elm_modelinfo.r1.ClassInfo;
 import org.hl7.elm_modelinfo.r1.TypeInfo;
 import org.hl7.fhir.r4.model.DateTimeType;
@@ -166,7 +166,7 @@ public class TestR4ModelResolver {
     public void modelInfo400Tests() {
         ModelResolver resolver = new R4FhirModelResolver(FhirContext.forCached(FhirVersionEnum.R4));
         ModelManager mm = new ModelManager();
-        Model m = mm.resolveModel(new VersionedIdentifier().withId("FHIR").withVersion("4.0.1"));
+        Model m = mm.resolveModel(new ModelIdentifier().withId("FHIR").withVersion("4.0.1"));
 
         List<TypeInfo> typeInfos = m.getModelInfo().getTypeInfo();
 
@@ -200,7 +200,7 @@ public class TestR4ModelResolver {
     public void modelInfo401Tests() throws Exception {
         ModelResolver resolver = new R4FhirModelResolver(FhirContext.forCached(FhirVersionEnum.R4));
         ModelManager mm = new ModelManager();
-        Model m = mm.resolveModel(new VersionedIdentifier().withId("FHIR").withVersion("4.0.1"));
+        Model m = mm.resolveModel(new ModelIdentifier().withId("FHIR").withVersion("4.0.1"));
 
         List<TypeInfo> typeInfos = m.getModelInfo().getTypeInfo();
 
