f13faa07b47e02a60040c7241fab6f92547085ea
Jonathan Percival <jonathan.i.percival@gmail.com>
2022-09-14 16:13:32 -0600

    Synchronize access to ServiceLoaders to prevent race conditions

--- engine/src/main/java/org/opencds/cqf/cql/engine/serializing/CqlLibraryReaderFactory.java
+++ engine/src/main/java/org/opencds/cqf/cql/engine/serializing/CqlLibraryReaderFactory.java
@@ -8,17 +8,20 @@ public class CqlLibraryReaderFactory {
     static ServiceLoader<CqlLibraryReaderProvider> loader = ServiceLoader
             .load(CqlLibraryReaderProvider.class);
 
-    public static Iterator<CqlLibraryReaderProvider> providers(boolean refresh) {
+    public static synchronized Iterator<CqlLibraryReaderProvider> providers(boolean refresh) {
         if (refresh) {
             loader.reload();
         }
+
         return loader.iterator();
     }
 
     public static CqlLibraryReader getReader(String contentType) {
-        if (providers(false).hasNext()) {
-            return providers(false).next().create(contentType);
+        var providers = providers(false);
+        if (providers.hasNext()) {
+            return providers.next().create(contentType);
         }
+
         throw new RuntimeException("No ElmLibraryReaderProviders found for " + contentType);
     }
 }
